5286912ad43909b3ef6b656f91e2dcb1d862db4c
Alexandre Dibon <alexandre.dibon@gmail.com>
2017-11-28 16:53:14 +0100

    final validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,6 +4,6 @@ class Review < ApplicationRecord
   belongs_to :sender, class_name: "Profil", foreign_key: "sender_id"
   belongs_to :recipient, class_name: "Profil", foreign_key: "recipient_id"
 
-  validates :rating, presence: true
+  validates :rating, numericality: { only_integer: true }, inclusion: { in: [0, 1, 2, 3, 4, 5], allow_nil: false }
   validates :description, presence: true
 end
