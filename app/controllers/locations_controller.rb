cb4520b7de2b2c9fb3327059040c0f21cb2cee90
Alexis Maerten <maerten.alexis@gmail.com>
2017-11-28 14:31:46 +0100

    location_controller fini

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,13 +1,35 @@
 class LocationsController < ApplicationController
+
+  before_action :set_location, only: [:show]
+
   def new
+    @locations = Location.new
   end
 
   def create
+    @location = Location.new(location_params)
+    if @location.save
+      redirect_to location_path(@location)
+    else
+      render :new
+    end
   end
 
   def show
   end
 
   def destroy
+    @location.destroy
+    redirect_to developpeurs_path
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:status, :check_in, :check_out, :recruteur_id, :developpeur_id )
+  end
+
+  def set_location
+    @location = Location.find(params[:id])
   end
 end
