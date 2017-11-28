83ecacbc99e60e95c6429cf675686cebd6823de7
Alexis Maerten <maerten.alexis@gmail.com>
2017-11-28 12:40:51 +0100

    add destroy_all models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Languages.destroy_all
+Profil.destroy_all
+Review.destroy_all
+Message.destroy_all
+User.destroy_all
+Location.destroy_all
+Recruteur.destroy_all
+Developpeur.destroy_all
+Skill.destroy_all
+Language.destroy_all
 
 languages = ['C', 'Java', 'Objective c', 'C++', 'C#', 'PHP', 'Visual Basic', 'Python', 'Transact SQL', 'JavaScript', 'Visual Basic', 'Perl', 'Ruby', 'Matlab', 'Delphi', 'Lips', 'PL/SQL', 'Pascal', 'Assembly', 'F#']
-languages.each { |languages| Languages.create(name: languages)}
+languages.each { |language| Language.create(name: language)}
 
