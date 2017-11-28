5f18ff947b5ade5c6a683abbf7780dcef1cd6304
Alexis Maerten <maerten.alexis@gmail.com>
2017-11-28 11:45:43 +0100

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Languages.destroy_all
+
+languages = ['C', 'Java', 'Objective c', 'C++', 'C#', 'PHP', 'Visual Basic', 'Python', 'Transact SQL', 'JavaScript', 'Visual Basic', 'Perl', 'Ruby', 'Matlab', 'Delphi', 'Lips', 'PL/SQL', 'Pascal', 'Assembly', 'F#']
+languages.each { |languages| Languages.create(name: languages)}
+
