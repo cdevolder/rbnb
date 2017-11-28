e86f491803d1ff4dfff3461c5fcdbb8127c2cf09
Alexandre Dibon <alexandre.dibon@gmail.com>
2017-11-28 17:34:12 +0100

    validations

--- app/models/profil.rb
+++ app/models/profil.rb
@@ -10,5 +10,5 @@ class Profil < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :city, presence: true
-  mount_uploader :photo, PhotoUploader
+
 end
