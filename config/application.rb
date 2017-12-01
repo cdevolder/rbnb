1ff5182b59d2ef810407ade640333df3bce9657c
Alexandre Dibon <alexandre.dibon@gmail.com>
2017-12-01 11:53:36 +0100

    ajax messages

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,7 @@ module Rbnb
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
